dfa9b903fc8e11d00e0e736bd488106a5ed4fdaf
aziz-aa11 <azizbeksadullaev475@gmail.com>
2025-11-13 19:19:41 +0500

    Обновили код

--- 11dars.py
+++ 11dars.py
@@ -16,43 +16,43 @@ elif choy or salat:
 print(f"Jami {narh} so`m")
 
 
-narh = 15000
-choy = True
-salat = False
-assorti = True
-non = 1
-kompot = 1
-if choy:
-    print("Mijoz choy oldi")
-    narh = narh + 5000
-if salat:
-    print("Mijoz salat oldi")
-    narh = narh + 12000
-if assorti:
-    print("Mijoz assorti oldi")
-    narh = narh + 22000
-if non:
-    print("Mijoz non oldi")
-    narh = narh + 5000
-if kompot:
-    print("Mijoz kompot oldi")
-    narh = narh + 8500
-print(f"Jami {narh}")
+# narh = 15000
+# choy = True
+# salat = False
+# assorti = True
+# non = 1
+# kompot = 1
+# if choy:
+#     print("Mijoz choy oldi")
+#     narh = narh + 5000
+# if salat:
+#     print("Mijoz salat oldi")
+#     narh = narh + 12000
+# if assorti:
+#     print("Mijoz assorti oldi")
+#     narh = narh + 22000
+# if non:
+#     print("Mijoz non oldi")
+#     narh = narh + 5000
+# if kompot:
+#     print("Mijoz kompot oldi")
+#     narh = narh + 8500
+# print(f"Jami {narh}")
 
 
-menu = ['osh', 'somsa', 'xonim', 'norin', 'shashlik']
-ovqat = input("Nima ovqat yeysiz? ")
-if ovqat.lower() in menu:
-    print("Buyurtma qabul qilindi")
-else:
-    print("Bunday ovqat yo`q")
+# menu = ['osh', 'somsa', 'xonim', 'norin', 'shashlik']
+# ovqat = input("Nima ovqat yeysiz? ")
+# if ovqat.lower() in menu:
+#     print("Buyurtma qabul qilindi")
+# else:
+#     print("Bunday ovqat yo`q")
 
 
 
-menu = ['osh', 'somsa', 'xonim', 'norin', 'shashlik']
-buyurtmalar = ['somsa', 'norin', 'manti', 'shashlik']
-for taom in buyurtmalar:
-    if taom in menu:
-        print(f"Menyuda {taom} bor")
-    else:
-        print(f"Menyuda {taom} yo`q")
+# menu = ['osh', 'somsa', 'xonim', 'norin', 'shashlik']
+# buyurtmalar = ['somsa', 'norin', 'manti', 'shashlik']
+# for taom in buyurtmalar:
+#     if taom in menu:
+#         print(f"Menyuda {taom} bor")
+#     else:
+#         print(f"Menyuda {taom} yo`q")
