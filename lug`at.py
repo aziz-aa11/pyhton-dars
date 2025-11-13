8e3a46382c0bbe332cd104deb6b95a09d325928e
aziz-aa11 <azizbeksadullaev475@gmail.com>
2025-11-13 21:45:30 +0500

    lug`at

---
+++ lug`at.py
@@ -0,0 +1,77 @@
+
+# car_0= {"model":"ferrari", "rang":"qizil"}
+# print(car_0['model'])
+# print(car_0['rang'])
+
+
+# en_uz = {"apple":"olma","orange":"apelsin"}
+# print(en_uz['apple'])
+
+# mevalar ={"olma":10000,"qovun":15000,"tarvuz":12500}
+# del mevalar['olma']
+# print(f"olma {mevalar['olma']} so`m")
+
+# talaba_0 = {"ism":"murod olimov", "tug`ilga yil":2003, "yosh":22}
+# print(f"{talaba_0['ism'].title()}, \
+# {talaba_0['tug`ilga yil']}-yilda tug`ilgan, \
+# {talaba_0['yosh']} yoshda")
+
+
+# talaba_1 ={}
+# talaba_1['ism'] = 'murod olimov'
+# talaba_1['kurs'] = 3
+# talaba_1['yosh'] = 21
+# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']} kurs")
+
+
+# telefonlar ={
+#     "Ali":"iphone 15",
+#     "Vali":"Redmi",
+#     "Hasan":"Samsung"
+#     }
+# #print(f"Ali da {telefonlar['Ali']} bor")
+
+
+# phone = telefonlar.get("Aziz","Azizda telefon yo`q")
+# print(phone)
+
+
+# otam = {'ismi':'mavlutdin', 'tyil':1954,'viloyat':'samarqand'}
+# tyil = otam['tyil']
+# vil = otam['viloyat']
+# print(f"Otamning ismi {otam['ismi'].title()}, {tyil}-yilda, {vil.title()} viloyatida tug'ilgan")
+
+
+
+
+# taomlar = {
+#     'ali':'osh',
+#     'vali':'shashlik',
+#     'hasan':"lag'mon",
+#     'husan':"mastava",
+#     'olim':"somsa"
+#     }
+
+# taom = taomlar['ali']
+# print(f"Alining sevimli taomi {taom}")
+
+# python_izohli_lugati = {
+#     'integer':"Butun son",
+#     'float':"O'nlik son",
+#     'string':"Matn",
+#     'list':"Ro'yxat",
+#     'tuple':"O'zgarmas ro'yxat"}
+# # print(python_izohli_lugati['tuple'])
+
+# kalit = input("Kalit so'z kiriting:").lower()
+# print(python_izohli_lugati.get(kalit,"Bunday so'z mavjud emas"))
+
+
+# kalit = input("Kalit so'z kiriting:").lower()
+# tarjima = python_izohli_lugati.get(kalit)
+# if tarjima==None:
+#     print("Bunday so'z mavjud emas")
+# else:
+#     print(f"{kalit.title()} so'zi {tarjima} deb tarjima qilinadi")
+
+
